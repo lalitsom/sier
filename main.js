5e429d08a844c81e8d331a120aed35663817012c
lalitsom <lalitsom27@gmail.com>
2019-03-13 01:06:51 +0530

    added more colors

--- main.js
+++ main.js
@@ -4,7 +4,10 @@ c=cnvs.getContext('2d');
 wd = cnvs.width;
 ht = cnvs.height;
 
-colors = ["red","blue","black","yellow","green"];
+colors = ['#e6194b', '#3cb44b', '#ffe119', '#4363d8', '#f58231', '#911eb4', '#46f0f0', '#f032e6',
+'#bcf60c', '#fabebe', '#008080', '#e6beff', '#9a6324', '#fffac8', '#800000', '#aaffc3', '#808000',
+'#ffd8b1', '#000075', '#808080', '#ffffff'];
+
 bndx = wd-10;
 bndy = ht-10;
 
